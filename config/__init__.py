178e201dc346e8c0203f681a772f683c45af235f
Yubaraj Shrestha <companion.krish@outlook.com>
2021-09-07 09:37:49 +0545

    Updated Environment

--- config/__init__.py
+++ config/__init__.py
@@ -1,23 +1,27 @@
 import os
+
 from config.database import database
+from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__name__))
+dotenv_path = os.path.join(basedir, '.env')
+load_dotenv(dotenv_path)
 
 
 class Config(object):
     DEBUG = False
-    KEY = os.environ.get('APP_KEY')
+    KEY = os.getenv('APP_KEY')
 
 
 # Create the development config
 class DevelopmentConfig(Config):
     DEBUG = True
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = os.environ.get('MAIL_PORT')
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
-    MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL')
+    MAIL_SERVER = os.getenv('MAIL_SERVER')
+    MAIL_PORT = os.getenv('MAIL_PORT')
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+    MAIL_USE_TLS = os.getenv('MAIL_USE_TLS')
+    MAIL_USE_SSL = os.getenv('MAIL_USE_SSL')
     ORATOR_DATABASES = database
 
 
@@ -27,9 +31,8 @@ class TestingConfig(Config):
     TESTING = True
     ORATOR_DATABASES = database
 
+
 # create the production config
-
-
 class ProductionConfig(Config):
     DEBUG = False
     ORATOR_DATABASES = database
--- config/database.py
+++ config/database.py
@@ -1,12 +1,19 @@
+import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__name__))
+dotenv_path = os.path.join(basedir, '.env')
+load_dotenv(dotenv_path)
+
 database = {
     'default': 'mysql',
 
     'mysql': {
         'driver': 'mysql',
-        'host': 'localhost',
-        'user': 'root',
-        'password': 'MDB@123#go',
-        'database': 'flask',
+        'host': os.getenv('DB_HOST', 'localhost'),
+        'user': os.getenv('DB_USER', 'root'),
+        'password': os.getenv('DB_PASSWORD', ''),
+        'database': os.getenv('DB_DATABASE'),
     },
 
     'postgres': {},
